1b5665d384a747bfaa9eeacbebaf2f3e22e53d13
yuma14 <yuma140902@gmail.com>
2023-03-15 01:28:29 +0900

    add ping-pong

--- main.ts
+++ main.ts
@@ -1,8 +1,47 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import * as discord from "https://deno.land/x/discordeno@18.0.1/mod.ts";
+import * as dotenv from "https://deno.land/std@0.179.0/dotenv/mod.ts";
+
+async function main() {
+  await dotenv.load({
+    export: true,
+    examplePath: "./.env.example",
+    envPath: "./.env",
+  });
+
+  const bot = discord.createBot({
+    token: Deno.env.get("DISCORD_TOKEN") ?? "(error)",
+    intents: discord.Intents.Guilds | discord.Intents.GuildMessages |
+      discord.Intents.MessageContent,
+  });
+
+  bot.events.ready = () => {
+    console.log("Successfully connected to gateway");
+  };
+  bot.events.messageCreate = (bot, message) => {
+    console.log("messageCreate", message);
+    if (message.content === "!ping") {
+      bot.helpers.addReaction(message.channelId, message.id, "✅");
+      bot.helpers.sendMessage(message.channelId, {
+        content: "Pong!",
+        messageReference: {
+          failIfNotExists: false,
+          guildId: message.guildId,
+          channelId: message.channelId,
+          messageId: message.id,
+        },
+      });
+    }
+    if (message.mentionedUserIds.includes(bot.id)) {
+      bot.helpers.addReaction(message.channelId, message.id, "👀");
+    }
+  };
+
+  await discord.startBot(bot);
 }
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  console.log("start");
+  await main();
+  console.log("bye");
 }
